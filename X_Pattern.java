9a3eebe9e6b77e6bf8d9964dc4e36a3f3dcd6edc
MadhuMaaligaShreeKS <163866846+MadhuMaaligaShreeKS@users.noreply.github.com>
2024-03-18 22:18:12 +0530

    Create X_Pattern.java

---
+++ X_Pattern.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+		String str="Program";
+        int len=str.length();
+        for (int i = 0; i < len; i++) { 
+            int j = len - 1 - i; 
+            for (int k = 0; k < len; k++) { 
+                if (k == i || k == j) 
+                    System.out.print(str.charAt(k)); 
+                else
+                    System.out.print(" "); 
+            } 
+            System.out.println(""); 
+		}
+	}
+}
